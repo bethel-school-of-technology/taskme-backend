41c95cb5d4432d43865ed96e81de33b3e9268b81
Michelle Huber <9modes@gmail.com>
2020-10-20 19:59:17 -0600

    setup first express project

---
+++ migrations/1-initial_migration.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * createTable "users", deps: []
+ *
+ **/
+
+var info = {
+    "revision": 1,
+    "name": "initial_migration",
+    "created": "2020-10-21T01:56:03.643Z",
+    "comment": ""
+};
+
+var migrationCommands = [{
+    fn: "createTable",
+    params: [
+        "users",
+        {
+            "UserId": {
+                "type": Sequelize.INTEGER,
+                "field": "UserId",
+                "primaryKey": true,
+                "autoIncrement": true,
+                "allowNull": false
+            },
+            "FirstName": {
+                "type": Sequelize.STRING,
+                "field": "FirstName"
+            },
+            "LastName": {
+                "type": Sequelize.STRING,
+                "field": "LastName"
+            },
+            "Email": {
+                "type": Sequelize.STRING,
+                "field": "Email",
+                "unique": true
+            },
+            "Username": {
+                "type": Sequelize.STRING,
+                "field": "Username",
+                "unique": true
+            },
+            "Password": {
+                "type": Sequelize.STRING,
+                "field": "Password"
+            },
+            "Deleted": {
+                "type": Sequelize.BOOLEAN,
+                "field": "Deleted",
+                "defaultValue": false
+            },
+            "Admin": {
+                "type": Sequelize.BOOLEAN,
+                "field": "Admin",
+                "defaultValue": false
+            },
+            "createdAt": {
+                "type": Sequelize.DATE,
+                "field": "createdAt",
+                "allowNull": false
+            },
+            "updatedAt": {
+                "type": Sequelize.DATE,
+                "field": "updatedAt",
+                "allowNull": false
+            }
+        },
+        {}
+    ]
+}];
+
+module.exports = {
+    pos: 0,
+    up: function(queryInterface, Sequelize)
+    {
+        var index = this.pos;
+        return new Promise(function(resolve, reject) {
+            function next() {
+                if (index < migrationCommands.length)
+                {
+                    let command = migrationCommands[index];
+                    console.log("[#"+index+"] execute: " + command.fn);
+                    index++;
+                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                }
+                else
+                    resolve();
+            }
+            next();
+        });
+    },
+    info: info
+};
---
+++ models/users.js
@@ -0,0 +1,47 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  users.init({
+    UserId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    FirstName: DataTypes.STRING,
+    LastName: DataTypes.STRING,
+    Email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    Username: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    Password: DataTypes.STRING,
+    Deleted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    Admin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+  }, {
+    sequelize,
+    modelName: 'users',
+  });
+  return users;
+};
