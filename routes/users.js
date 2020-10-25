abc96f34fd07d89d517b4b72472743be95e80de2
James Troughton <jamesstroughton@gmail.com>
2020-10-25 14:57:35 +1300

    Updated users route = arrow funcs + res.json

--- routes/users.js
+++ routes/users.js
@@ -1,108 +1,113 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var models = require('../models');
-var authService = require('../services/auth');
+var models = require("../models");
+var authService = require("../services/auth");
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get("/", (req, res, next) => {
+  res.json({message: "respond with a resource"});
 });
 
 // Create new user if one doesn't exist
-router.post('/signup', function (req, res, next) {
+router.post("/signup", (req, res, next) => {
   models.users
     .findOrCreate({
       where: {
-        Username: req.body.username
+        Username: req.body.username,
       },
       defaults: {
         FirstName: req.body.firstName,
         LastName: req.body.lastName,
         Email: req.body.email,
-        Password: authService.hashPassword(req.body.password)
-      }
+        Password: authService.hashPassword(req.body.password),
+      },
     })
     .spread(function (result, created) {
       if (created) {
-        res.send('User successfully created');
+      res.json({ message: "User successfully created"});
       } else {
-        res.send('This user already exists');
+        res.json({ message: "This user already exists"});
       }
     });
 });
 
 // Login user and return JWT as cookie
-router.post('/login', function (req, res, next) {
-  models.users.findOne({
-    where: {
-      Username: req.body.username
-    }
-  }).then(user => {
-    if (!user) {
-      console.log('User not found')
-      return res.status(401).json({
-        message: "Login Failed"
-      });
-    } else {
-      let passwordMatch = authService.comparePasswords(req.body.password, user.Password);
-      if (passwordMatch) {
-        let token = authService.signUser(user);
-        res.cookie('jwt', token);
-        res.send('Login successful');
+router.post("/login", (req, res, next) => {
+  models.users
+    .findOne({
+      where: {
+        Username: req.body.username,
+      },
+    })
+    .then((user) => {
+      if (!user) {
+        console.log("User not found");
+        return res.status(401).json({
+          message: "Login failed, did not match any records.",
+        });
       } else {
-        console.log('Wrong password');
-        res.send('Wrong password');
+        let passwordMatch = authService.comparePasswords(
+          req.body.password,
+          user.Password
+        );
+        if (passwordMatch) {
+          let token = authService.signUser(user);
+          res.cookie("jwt", token, { httpOnly: true });
+          res.json({ user, token });
+        } else {
+          res.json({
+            message: "Email and Password did not match any records.",
+          });
+        }
       }
-    }
-  });
+    });
 });
 
-router.get('/profile', function (req, res, next) {
+router.get("/profile", (req, res, next) => {
   let token = req.cookies.jwt;
   if (token) {
-    authService.verifyUser(token)
-      .then(user => {
-        if (user) {
-          res.send(JSON.stringify(user));
-        } else {
-          res.status(401);
-          res.send('Invalid authentication token');
-        }
-      });
+    authService.verifyUser(token).then((user) => {
+      if (user) {
+        res.send(JSON.stringify(user));
+      } else {
+        res.status(401);
+        res.json({ message: "Invalid authentication token" });
+      }
+    });
   } else {
     res.status(401);
-    res.send('Must be logged in');
+    res.json({ message: "Must be logged in" });
   }
 });
 
-router.get('/profile/:id', authService.verifyUser, function(req, res, next) {
+router.get("/profile/:id", authService.verifyUser, (req, res, next) => {
   // if (!req.isAuthenticated()) {
   //   return res.send('You are not authenticated');
   // }
   if (req.params.id !== String(req.user.UserId)) {
-    res.send('This is not your profile');
+    res.json({ message: "This is not your profile"});
   } else {
     let status;
     if (req.user.Admin) {
-      status = 'Admin';
+      status = "Admin";
     } else {
-      status = 'Normal user';
+      status = "Normal user";
     }
 
-    res.render('profile', {
+    res.json({
       FirstName: req.user.FirstName,
       LastName: req.user.LastName,
       Email: req.user.Email,
       UserId: req.user.UserId,
       Username: req.user.Username,
-      Status: status
+      Status: status,
     });
   }
 });
 
-router.get('/logout', function (req, res, next) {
-  res.cookie('jwt', "", { expires: new Date(0) });
-  res.send('Logged out');
+router.get("/logout", (req, res, next) => {
+  res.cookie("jwt", "", { expires: new Date(0) });
+  res.json({ message: "Logged out" });
 });
 
 module.exports = router;
