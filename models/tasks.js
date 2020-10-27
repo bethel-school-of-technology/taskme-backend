bc68613db6ada75e19dc18d843c326e2d50507f3
James Troughton <jamesstroughton@gmail.com>
2020-10-28 00:40:02 +1300

    Added association

--- models/tasks.js
+++ models/tasks.js
@@ -9,10 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      // tasks.belongsTo(models.users, {
-      //   foreignKey: "ownedBy",
-      //   as: "user",
-      // });
+      tasks.belongsTo(models.users, {
+        foreignKey: "ownedBy",
+        as: "user",
+      });
     }
   }
   tasks.init(
